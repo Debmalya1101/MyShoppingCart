4e708236989f46e0634deaa4c4cd5167e34517f5
Debmalya Dutta <debmalya_dutta@persistent.com>
2022-12-20 11:03:57 +0530

    view details almost done, enlarging the database for view details is ongoing, entering data into database is ongoing

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -5,11 +5,30 @@ export class Product {
     price:number;
     imageUrl:string;
 
-    constructor(id=0,name='', description='', price=0, imageUrl='https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/71PvHfU+pwL._AC_UY327_FMwebp_QL65_.jpg'){
+    details:string;
+    os:string;
+    processor:string;
+    ram_rom:string;
+    rear_camera:string;
+    front_camera:string;
+    display:string;
+    battery:string;
+
+    constructor(id=0,name='', description='', price=0, imageUrl='https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/71PvHfU+pwL._AC_UY327_FMwebp_QL65_.jpg',
+                deatails='',os='',processor='',ram_rom='',rear_camera='',front_camera='', display='',battery=''){
         this.id=id
         this.name=name
         this.description=description
         this.price=price
         this.imageUrl=imageUrl
+
+        this.details=deatails
+        this.os=os;
+        this.processor=processor
+        this.ram_rom=ram_rom
+        this.rear_camera=rear_camera
+        this.front_camera=front_camera
+        this.display=display
+        this.battery=battery
     }
 }
