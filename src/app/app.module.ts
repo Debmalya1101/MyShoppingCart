4fe6c90d71b66b7b86b50c86ca7564f8ec90b5c8
Debmalya Dutta <debmalya_dutta@persistent.com>
2023-03-29 14:11:27 +0530

    Search bar added, no records found added, for Search Bar the Pagination in Home page is changed to ngx-pagination from my custom pagination, Encryption of password at frontend will start from here

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,6 +34,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { JwtModule } from '@auth0/angular-jwt';
 
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { Ng2SearchPipe } from 'ng2-search-filter';
+
 
 @NgModule({
   declarations: [
@@ -69,6 +72,7 @@ import { JwtModule } from '@auth0/angular-jwt';
     ReactiveFormsModule,
     CommonModule,
     BrowserAnimationsModule, // required animations module
+    Ng2SearchPipeModule,
     ToastrModule.forRoot(),
     JwtModule.forRoot({
       config: {
--- src/app/components/shopping-cart/product-list/product-list.component.ts
+++ src/app/components/shopping-cart/product-list/product-list.component.ts
@@ -4,17 +4,18 @@ import { FiltermessengerService } from './../../../services/filtermessenger.serv
 import { User } from 'src/app/models/user';
 import { WishlistService } from './../../../services/wishlist.service';
 import { Product } from './../../../models/product';
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, Component, OnInit, ViewChildren, ChangeDetectorRef } from '@angular/core';
 
 import { ProductService } from 'src/app/services/product.service';
 import { JwtHelperService } from '@auth0/angular-jwt';
+
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, AfterContentChecked {
 
   productList: Product[] = []
   wishlist: number[] = []
@@ -23,7 +24,7 @@ export class ProductListComponent implements OnInit {
 
   //added for pagination
   products!: Product[]
-  productsPerPage: number = 3;
+  productsPerPage: number = 6;
   productListLength!: number;
   public selectedPage = 1
   //ends here
@@ -31,16 +32,27 @@ export class ProductListComponent implements OnInit {
 
   //for filter pagination
   isfilter:boolean=false
-  filterProductsPerPage = 3;
+  filterProductsPerPage = 6;
   filterProductListLength!: number;
   public filterSelectedPage = 1
+
+  //this variable is added for Search bar input field
+  term!: string
+  p:number=1;
+
+  @ViewChildren('someVar') filteredItems: any;
 
   constructor(private productService: ProductService,
     private wishlistService: WishlistService,
     private filtermsg: FiltermessengerService,
     private resetpricefiltermsg: ResetpricefiltermessengerService,
-    public jwtHelper: JwtHelperService
+    public jwtHelper: JwtHelperService,
+    private changeDetector: ChangeDetectorRef,
   ) { }
+
+  ngAfterContentChecked(): void {
+    this.changeDetector.detectChanges();
+  }
 
   ngOnInit(): void {
     // this.productList=this.productService.getProducts()
@@ -134,11 +146,14 @@ export class ProductListComponent implements OnInit {
   //wishlist function
   loadWishlist() {
     this.user = this.jwtHelper.decodeToken(JSON.parse(sessionStorage.getItem('token')!))!
-    console.log(this.user)
     // this.user = JSON.parse(localStorage.getItem('user')!)
+
+    //for the npxPagination module this line is added
+    this.wishlist=[]
+
     this.wishlistService.getWishlist(this.user).subscribe(data => {
       data.forEach((w: any) => this.wishlist.push(w.productId));
-      //console.log(this.wishlist)
+      console.log(this.wishlist)
     })
   }
 
